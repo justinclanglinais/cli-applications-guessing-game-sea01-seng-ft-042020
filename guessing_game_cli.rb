b885cc7c59d1554f124c45b850ab9d138ab88245
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-14 18:24:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 require 'pry'
 def run_guessing_game
-  rando = (rand(6) + 1)
+  rando = (rand(6) + 1).to_s
   input = gets.chomp
   if input == "exit"
     puts "Goodbye!"
